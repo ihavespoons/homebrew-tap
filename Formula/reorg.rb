8b8703fc11c793155563e21784515db706f7414b
Ben Gittins <ihavespoons@users.noreply.github.com>
2026-01-22 17:18:38 +1000

    Updating Github usernames

--- Formula/reorg.rb
+++ Formula/reorg.rb
@@ -6,27 +6,27 @@
 
 class Reorg < Formula
   desc "Personal organization tool for managing areas, projects, and tasks"
-  homepage "https://github.com/beng/reorg"
+  homepage "https://github.com/ihavespoons/reorg"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/beng/reorg/releases/download/v0.1.0/reorg_0.1.0_darwin_amd64.tar.gz"
+      url "https://github.com/ihavespoons/reorg/releases/download/v0.1.0/reorg_0.1.0_darwin_amd64.tar.gz"
       sha256 "PLACEHOLDER_SHA256_DARWIN_AMD64"
     end
     on_arm do
-      url "https://github.com/beng/reorg/releases/download/v0.1.0/reorg_0.1.0_darwin_arm64.tar.gz"
+      url "https://github.com/ihavespoons/reorg/releases/download/v0.1.0/reorg_0.1.0_darwin_arm64.tar.gz"
       sha256 "PLACEHOLDER_SHA256_DARWIN_ARM64"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/beng/reorg/releases/download/v0.1.0/reorg_0.1.0_linux_amd64.tar.gz"
+      url "https://github.com/ihavespoons/reorg/releases/download/v0.1.0/reorg_0.1.0_linux_amd64.tar.gz"
       sha256 "PLACEHOLDER_SHA256_LINUX_AMD64"
     end
     on_arm do
-      url "https://github.com/beng/reorg/releases/download/v0.1.0/reorg_0.1.0_linux_arm64.tar.gz"
+      url "https://github.com/ihavespoons/reorg/releases/download/v0.1.0/reorg_0.1.0_linux_arm64.tar.gz"
       sha256 "PLACEHOLDER_SHA256_LINUX_ARM64"
     end
   end
